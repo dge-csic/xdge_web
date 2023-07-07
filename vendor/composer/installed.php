bad4f1f0cd8a2a2bb27008527cf6c03d0fbf3a4c
Frédéric Glorieux <frederic.glorieux@fictif.org>
2023-07-07 19:10:58 +0200

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '963a6307e5000487809180e2843131b42c1be24a',
+        'name' => 'dge-csic/teinte_galenus',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dge-csic/teinte_galenus' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '963a6307e5000487809180e2843131b42c1be24a',
+            'dev_requirement' => false,
+        ),
+        'oeuvres/kit' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../oeuvres/kit',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '5e1b8b7f91ad487208452de9786f074bc45e6163',
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+            'dev_requirement' => false,
+        ),
+    ),
+);
